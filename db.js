b7aac93c8b3237f1c15ef805b404b20918c8afab
Dv <dipvachhani1111@gmail.com>
2020-06-27 22:19:33 +0530

    add db

--- db.js
+++ db.js
@@ -1,19 +1,22 @@
 //import mongoose library
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-//MONGO_URI 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/url'; 
+//MONGO_URI
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  "mongodb+srv://admin:Dip.15739@cluster0-drsba.mongodb.net/<url>?retryWrites=true&w=majority";
 
 //Connect to DB function
 const connect = () => {
-  mongoose.connect(MONGO_URI, {useNewUrlParser : true, useUnifiedTopology : true})
-  .then(() => console.log('DB connected'))
-  .catch(err => console.log(err));
+  mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log(err));
   //On connection error, log the message
-  mongoose.connection.on('error', err => {
+  mongoose.connection.on("error", (err) => {
     console.log(`DB connection error : ${err.message}`);
   });
-}
+};
 
 //export the connect function, to use in server.js
-module.exports = { connect }; 
+module.exports = { connect };
