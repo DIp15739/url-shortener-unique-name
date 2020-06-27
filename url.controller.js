99fdd77fd43ecde5560697ee935b2edbad5402f5
Dv <dipvachhani1111@gmail.com>
2020-06-27 23:02:08 +0530

    add heroku url as domain

--- url.controller.js
+++ url.controller.js
@@ -2,7 +2,7 @@
 const Url = require('./url.model.js');
 
 //This is basically your domain name
-const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+const baseUrl = process.env.BASE_URL || 'https://url-shortener-unique-name.herokuapp.com';
 
 const createShortLink = async (req, res) => {
     //get the originalUrl and unique_name from the request's body
